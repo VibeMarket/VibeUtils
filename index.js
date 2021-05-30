c5865ca9bfa7cb2fa23fb2e7e3a12d5656c78bf7
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-05-29 21:07:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const {prefix, token, ownerID} = require('./config/config.json');
+const {prefix, ownerID, reactEmoji, token} = require('./config/config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+require('dotenv').config();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     client.commands.set(command.name, command);
 }
 
 client.on('message', message => {
+    if ('welcome' === message.content.toLowerCase()) {
+     message.react(reactEmoji)
+    }
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
@@ -26,9 +29,7 @@ client.on('message', message => {
         console.error(error);
         message.reply('There was an error while trying to execute that command!');
     }
-
 });
-
 client.once("ready", () => {
     client.user.setActivity('you | +help', {
         type: "WATCHING"
