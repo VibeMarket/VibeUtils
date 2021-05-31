6de4aac8007079ae6c6e6353effae245a4dd62e9
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-05-31 02:11:47 -0400

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { prefix } = require('../config/config.json');
+module.exports = {
+    name: 'serverinfo',
+    description: 'Shows serverinfo',
+    aliases: ['s'],
+    execute(message) {
+        const serverInfoEmbed = new Discord.MessageEmbed()
+        .setColor('#9da9f0')
+        .addFields(
+           {name: "Vibe Members", value: `${message.guild.memberCount}`},
+           {name: "Vibe Rules Channel", value: `<!#${message.guild.rulesChannelID}>`},
+           {name: "Vibe Owner", value: `<!@${message.guild.ownerID}>`},
+        )
+        .setFooter("Made by Cryptic#0001 | © Vibe Market")
+     message.channel.send(serverInfoEmbed);
+    }
