f94403fd1869b7ec7c939f5fd4e21640ca5657e8
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2021-06-29 21:57:16 -0400

    Remove line that wants dotenv

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const {prefix, ownerID, reactEmoji, token} = require('./config/config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-require('dotenv').config();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
