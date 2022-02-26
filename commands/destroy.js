4c3f6ea6628366a2ed865443357e4700bb1d6891
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2022-02-25 20:07:36 -0500

    Fix destroy command
    Signed-off-by: Kevin D <43621653+ItsCryptic@users.noreply.github.com>

--- commands/destroy.js
+++ commands/destroy.js
@@ -3,10 +3,14 @@ const { ownerID } = require('../config/config.json');
 module.exports = {
     name: 'destroy',
     description: 'Takes the bot offline. Limited to only be used by Kevin.',
-    aliases: ["de"],
     execute(message) {
         if (message.author.id == ownerID) {
-            process.exit();
+        message.channel.send(`Terminating... If you did not fuck up, I should come back online.`)
+        .then(()=> {
+            setTimeout(function(){
+            process.exit()
+            }, 1000)
+        }); 
         }else {
             message.channel.send("You don't have permission to use this command. Please contact " + `<@${ownerID}> if you believe you should be able to use this command (which you're definitely not supposed to be able too)`).then(message => message.delete({timeout: 10000}));
         }
