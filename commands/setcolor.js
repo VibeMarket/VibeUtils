2b093cc50edfed67107ddb98d577f0c194b2851f
Kevin D <kevin.diangelis@gmail.com>
2022-03-02 13:24:27 -0500

    Lots of cleanup for this will need to take place... eventually

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -8,7 +8,7 @@ module.exports = {
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         if (!args.length()) message.channel.send("Please specify a color.");
         if (message.member.roles.has("Booster")) {
-            if (message.guild.roles.cache.find(role => role.name == message.author)) role.edit({ name: message.author, color: "#" + args[1] });
+            if (message.guild.roles.cache.find(role => role.name == message.author)) role.edit({ name: message.author, color: "#" + args[1]});
             else {
                 message.guild.roles.create({data: {name: message.author, color: args[1], permissions: 0}}).then (role => {message.members.roles.add(role); message.channel.send("I've created the role " + role + " and I have given you it. If you experience any issues with this command, please contact Kevin.")})};
         }else {
