b205aed83add1a89e2b7f74662efcb447d9cacb3
Kevin D <43621653+ItsCryptic@users.noreply.github.com>
2022-03-02 20:04:59 -0500

    Fix a lot of issues with setcolor
    Signed-off-by: Kevin D <43621653+ItsCryptic@users.noreply.github.com>

--- commands/setcolor.js
+++ commands/setcolor.js
@@ -1,18 +1,21 @@
 const Discord = require('discord.js');
-const { prefix } = require('./config/config.json');
+const { prefix } = require('../config/config.json');
 module.exports = { 
     name: 'setcolor',
     args: true,
     description: 'Set role color. Limited to boosters.',
     execute(message) {
         const args = message.content.slice(prefix.length).trim().split(/ +/);
-        if (!args.length()) message.channel.send("Please specify a color.");
-        if (message.member.roles.has("Booster")) {
-            if (message.guild.roles.cache.find(role => role.name == message.author)) role.edit({ name: message.author, color: "#" + args[1]});
+        //let role = message.guild.roles.find(x => x.name == message.author.username);
+        if (message.member.roles.cache.some(role => role.name === '✭ Booster')) {
+            if (!args[1]) message.channel.send("Please specify a color.");
+            let role1 = message.guild.roles.cache.find(role => role.name === message.author.username);
+            if (role1) role1.edit({ name: message.author.username, color: args[1]});
             else {
-                message.guild.roles.create({data: {name: message.author, color: args[1], permissions: 0}}).then (role => {message.members.roles.add(role); message.channel.send("I've created the role " + role + " and I have given you it. If you experience any issues with this command, please contact Kevin.")})};
+                message.guild.roles.create(
+                    {data: {name: message.author.username, color: args[1], permissions: 0}}).then(role => {role.setPosition(100); message.member.roles.add(role)}); message.channel.send("I've created the role and I have given you it. If you experience any issues with this command, please contact Kevin.");};
         }else {
-            message.channel.send("You have to be a Booster to use this command.");
+            message.channel.send("You have to be a ```✭ Booster``` to use this command.");
         }
     }
 }
