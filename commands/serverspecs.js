1dc472227424b871691258fe7fd2a5a4d61831a9
Kevin D <kevin.diangelis@gmail.com>
2024-03-24 15:48:11 -0400

    new server specs

--- commands/serverspecs.js
+++ commands/serverspecs.js
@@ -9,7 +9,7 @@ module.exports = {
         
         let serverSpecs = new MessageEmbed()
             .setTitle("<:icons_globe:859424401971609600> Our server specs.")
-            .setDescription("<:icons_text5:920259712728072252> Intel(R) Xeon(R) CPU E5-2630 v4 @ 2.20 GHz\n<:icons_text5:920259712728072252> vCores: 6\n<:icons_text5:920259712728072252> RAM: 16GB DDR4\n<:icons_text5:920259712728072252> Disk Space: 400GB\n<:icons_text5:920259712728072252> Uplink: 400mb/s\n<:icons_text5:920259712728072252> OS: Ubuntu 20.04")
+            .setDescription("<:icons_text5:920259712728072252> Intel(R) Xeon(R) CPU E5-2680 v2 @ 2.80 GHz\n<:icons_text5:920259712728072252> Cores: 20\n<:icons_text5:920259712728072252> RAM: 256GB DDR3\n<:icons_text5:920259712728072252> Disk Space: 40TB\n<:icons_text5:920259712728072252> Uplink: 10gb/s\n<:icons_text5:920259712728072252> OS: Proxmox 8")
         await interaction.reply({ embeds : [serverSpecs] })
     }
 }
